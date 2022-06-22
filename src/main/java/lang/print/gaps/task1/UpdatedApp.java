adb824b888050e7ebea436b5868035f4010c6357
pasatigiy <82204580+pasatigiy@users.noreply.github.com>
2022-06-22 11:24:19 +0300

    Update UpdatedApp.java

--- src/main/java/lang/print/gaps/task1/UpdatedApp.java
+++ src/main/java/lang/print/gaps/task1/UpdatedApp.java
@@ -1,6 +1,6 @@
 package lang.print.gaps.task1;
 
-public class UpdateApp {
+public class UpdatedApp {
     public static void main(String[] args) {
         System.out.println("I'm updated");
         System.out.println("can be compiled. And can write to console");
